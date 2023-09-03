3298d3f55bb6bbba0cc88334ee573249fc668679
Lukas Apalovic <lapalovic@sygic.com>
2023-09-03 15:55:01 +0200

    version 1.1.0 fix release icon

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -2,6 +2,6 @@ object Config {
 
     const val compileSdkVersion = 33
     const val minSdkVersion = 24
-    const val versionName = "1.0.0"
-    const val versionCode = 2
+    const val versionName = "1.1.0"
+    const val versionCode = 4
 }
