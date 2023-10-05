54a094a06985837c7286908eb0be66d8f5d47b2b
Lukas Apalovic <lapalovic@sygic.com>
2023-10-05 18:25:28 +0200

    version 1.2.1

--- buildSrc/src/main/java/Config.kt
+++ buildSrc/src/main/java/Config.kt
@@ -2,6 +2,6 @@ object Config {
 
     const val compileSdkVersion = 33
     const val minSdkVersion = 24
-    const val versionName = "1.2.0"
-    const val versionCode = 5
+    const val versionName = "1.2.1"
+    const val versionCode = 6
 }
